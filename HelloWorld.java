09c5d962e57f5fc019eb4bc1b1d80b938c441e58
Nilesh Rothe <91383758+nrothe1998@users.noreply.github.com>
2022-03-03 16:12:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
   
   public static void main(String args[]) {
     System.out.println("Hello Jenkins i am from github!!!!");
+    int a=10;
+    int b=20;
+    int sum =a+b;
+     System.out.println(sum);
   }
 }
